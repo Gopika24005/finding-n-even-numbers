cb6e496746022d2bada28dcfee590317db3950a8
GOPIKA <gopikasaravanan81@gmail.com>
2024-10-07 14:32:23 +0530

    Create even no.java

---
+++ even no.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main
+{
+	public static void main(String[] args) 
+	{
+		int n=20;
+	    for(int i=1;i<=n;i++)
+	    {
+	       if(i%2 ==0)
+	         System.out.println(i);
+	        
+	    }
+	}
+}
